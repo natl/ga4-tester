d473cd641872df82035db443b92afb4957f1dc02
natl <natl@users.noreply.github.com>
2021-07-09 20:21:41 +1000

    add GTM noscript

--- public/js/ga.js
+++ public/js/ga.js
@@ -59,14 +59,16 @@ function closeModal() {
     } else {
         // cookie exists
         const gtm_code = getCookie('gtm');
-        const script = document.createElement('script');
         // script.innerHTML = `
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer',gtm_code);
-// `;
-    document.head.appendChild(script);
+    const noscript = document.createElement('noscript');
+    noscript.innerHTML = '<iframe src="https://www.googletagmanager.com/ns.html?id="' + gtm_code + 'height="0" width="0" style="display:none;visibility:hidden"></iframe>';
+    document.body.insertBefore(noscript, document.body.firstChild);
+
     }
+
 //   });
